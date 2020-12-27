075eae4505064da2113c1ec4415ad28ca1fddb81
kylak <gustavberloty2@gmail.com>
2020-12-28 00:17:17 +0100

    dataToTest.java implemented

--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@ Here is a command to download the public CNTR collation :
 javac UpdateData.java -d Diverse/Generated\ Java\ classes/ && cd Diverse/Generated\ Java\ classes/ && java UpdateData && sh .downloadData  && rm .downloadData && cd ../
 ```
 The CNTR downloaded files are located on ```/Data/CNTR/$TODAYSDATE$```
+
+Here is a command to download others data than the CNTR ones : 
+```
+javac dataToTest.java -d Diverse/Generated\ Java\ classes/ && cd Diverse/Generated\ Java\ classes/ && java dataToTest && sh .downloadDataToCompare  && rm .downloadDataToCompare && cd ../..
+```
+These datas files are located on ```/Data/dataToTest/$TODAYSDATE$```
--- dataToTest.java
+++ dataToTest.java
@@ -8,15 +8,56 @@ public class dataToTest {
     String urlBase = "/Users/gustavberloty/Documents/GitHub/comparingCNTRdata/";
     
     public dataToTest() {
+    
+    	String[] command = new String[11];
+        for (int i = 0; i < command.length; i++) {
+            command[i] = "";
+        }
         
         String url = urlBase + "Data/dataToTest/";
         String newFolderName = "\"`date +%Y-%m-%d` - not (yet) fully downloaded\"";
+        command[0] += "mkdir " + newFolderName + "\n\n";
+		
+		String websiteBase = "https://verite.github.io/bible-grec-interligne/bcgr/"; // forme : livre-chapitre.html
+		
+		for (int book = 40; book <= 66; book++) {
+            switch (book) {
+                case 40 : case 44 :
+                    command[0] += "for i in {1..9}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\nfor i in {10..28}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 41 : case 45 : case 46 :
+                    command[1] += "for i in {1..9}; do curl" + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\nfor i in {10..16}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 42 :
+                    command[2] += "for i in {1..9}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html;done;\nfor i in {10..24}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 43 :
+                    command[3] += "for i in {1..9}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\nfor i in {10..21}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 47 : case 58 :
+                    command[4] += "for i in {1..9}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\nfor i in {10..13}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 48 : case 49 : case 54 :
+                    command[5] += "for i in {1..6}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 50 : case 51 : case 55 :
+                    command[6] += "for i in {1..4}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 52 : case 59 : case 60 : case 62 :
+                    command[7] += "for i in {1..5}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 53 : case 56 : case 61 :
+                    command[8] += "for i in {1..3}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+                case 57 : case 63 : case 64 : case 65 :
+                    command[9] += "curl " + websiteBase + (book - 39) + "001.html >" + url + newFolderName + "/" + (book - 39) + "001.html;\n";
+                break;
+                case 66 :
+                    command[10] += "for i in {1..9}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\nfor i in {10..22}; do curl " + websiteBase + (book - 39) + "-${i}.html >" + url + newFolderName + "/" + (book - 39) + "-${i}.html; done;\n";
+                break;
+            }
+        }
 
-		/*
-		
-			YOUR CODE HERE.
-		
-		*/
 
         try {
 			  FileWriter myWriter = new FileWriter(".downloadDataToCompare");
