034ba2477c68378170e7b49681d9d7c737ddca8f
kylak <gustavberloty2@gmail.com>
2020-12-27 22:40:38 +0100

    Create compareData.java

---
+++ compareData.java
@@ -0,0 +1,21 @@
+import java.io.File;  // Import the File class
+import java.io.IOException;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.FilenameFilter;
+
+public class compareData {
+    
+    String urlBase = "/Users/gustavberloty/Documents/GitHub/comparingCNTRdata/";
+    
+    public compareData() {
+        
+        // THE FUTURE CODE HERE.
+        
+    }
+        
+    public static void main(String[] args) {
+    
+    	new compareData();
+    }
+}
+    
